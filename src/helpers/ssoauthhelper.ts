0c12191fe43e076bcadff966dd781c091c3972b6
MinhHoan147 <72193415+MinhHoan147@users.noreply.github.com>
2022-01-03 23:31:36 +0700

    Update ssoauthhelper.ts

--- src/helpers/ssoauthhelper.ts
+++ src/helpers/ssoauthhelper.ts
@@ -47,6 +47,7 @@ export async function getGraphData(): Promise<void> {
   }
 }
 
+//Function To Handel Error
 function handleAADErrors(exchangeResponse: any): void {
   // On rare occasions the bootstrap token is unexpired when Office validates it,
   // but expires by the time it is sent to AAD for exchange. AAD will respond
