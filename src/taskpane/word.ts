4dae5f614d8bacc260bd1a85d71beb2a599b863e
MinhHoan147 <72193415+MinhHoan147@users.noreply.github.com>
2022-01-15 19:01:19 +0700

    Update word.ts

--- src/taskpane/word.ts
+++ src/taskpane/word.ts
@@ -14,7 +14,7 @@ Office.onReady((info) => {
     });
   }
 });
-//wri
+//
 export function writeDataToOfficeDocument(result: Object): Promise<any> {
   return Word.run(function (context) {
     let myData: string[] = [];
